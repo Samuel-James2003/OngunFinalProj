1f174236436c115b982f53faa2dd7956c344405e
Samuel James <64286580+Samuel-James2003@users.noreply.github.com>
2023-10-17 18:51:52 +0200

    Comments, server.js launches index,  packages

--- Server.js
+++ Server.js
@@ -1,1 +1,33 @@
-//Test What happens hi
+const app = require("express")();
+const express = require("express");
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const portnum = 3000;
+
+//Creation du serveur sur localhost
+app.use(express.static(__dirname + '/images'));
+
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/index.html`);
+});
+
+io.on("connection", (socket) => {
+    console.log("Utilisateur s'est connecté");
+
+    //On disconnect
+    socket.on("disconnect", () => {
+        console.log("Utilisateur s'est déconnecté");
+    });
+
+    //On message received
+    socket.on("chat message", (msg) => {
+        console.log("Message", msg);
+        io.emit("chat message", msg);
+    });
+});
+
+server.listen(portnum, () => {
+    console.log(`http://localhost:${portnum}/`);
+    console.log("serveur a l'ecoute");
+
+});
