c1b89c3826195d2fcf6cfd450ed1d85269f49d55
Samuel James <64286580+Samuel-James2003@users.noreply.github.com>
2023-10-20 10:02:52 +0200

    fix form names and added login

--- VersionPHP/php/index.php
+++ VersionPHP/php/index.php
@@ -47,8 +47,7 @@
     <!-- Navbar for both pages -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">Menu</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
@@ -72,7 +71,7 @@
     <!-- Entry Page Content -->
     <div class="container">
         <center>
-            <h1>Welcome to Our Website</h1>
+            <h1>Welcome to Your Website</h1>
         </center>
         <p>This is the entry page content.</p>
     </div>
@@ -85,16 +84,17 @@
             <button id="close-popup" class="close-button">&#215;</button>
             <br>
             <h2>Login or Register</h2>
-            <form id="login-form">
+            <form id="login-form" method="post">
                 <div class="form-group">
                     <label for="email">Email:</label>
-                    <input type="email" class="form-control" id="email" placeholder="Enter your email">
+                    <input type="email" class="form-control" id="email-login" placeholder="Enter your email">
                 </div>
                 <div class="form-group">
                     <label for="password">Password:</label>
                     <input type="password" class="form-control" id="password" placeholder="Enter your password">
                 </div>
-                <button type="submit" class="btn btn-primary">Login</button>
+                <input type="hidden" name="login" value="login">
+                <button type="submit" class="btn btn-primary" name="submit" value="login">Login</button>
             </form>
             <form id="register-form" style="display: none;">
                 <div class="form-group">
@@ -103,8 +103,7 @@
                 </div>
                 <div class="form-group">
                     <label for="password-register">Password:</label>
-                    <input type="password" class="form-control" id="password-register"
-                        placeholder="Enter your password">
+                    <input type="password" class="form-control" id="password-register" placeholder="Enter your password">
                 </div>
                 <div class="form-group">
                     <label for="custom-file">Place your eid file here:</label>
@@ -113,14 +112,16 @@
                         <label class="custom-file-label" for="customFile">Choose file</label>
                     </div>
                 </div>
-                <button type="submit" class="btn btn-success">Register</button>
+                <input type="hidden" name="register" value="register">
+                <button type="submit" class="btn btn-success" name="submit" value="register">Register</button>
             </form>
             <form id="forgotpassword-form" style="display: none;">
                 <div class="form-group">
                     <label for="email-register">Email:</label>
                     <input type="email" class="form-control" id="email-register" placeholder="Enter your email">
                 </div>
-                <button type="submit" class="btn btn-warning">Send new password</button>
+                <input type="hidden" name="forgotpass" value="forgotpass">
+                <button type="submit" class="btn btn-warning" name="submit" value="forgotpass">Send new password</button>
             </form>
             <button id="switch-to-register" class="btn btn-link">Switch to Register</button>
             <button id="switch-to-login" class="btn btn-link" style="display: none;">Switch to Login</button>
@@ -133,29 +134,33 @@
     <!-- Add Bootstrap JS and jQuery for popup functionality -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.min.js"></script>
+    <div>
+    
+        <!-- PHP Code -->
+        <?php
+        //session_start();
+        $servername = 'localhost';
 
-    <!-- PHP Code -->
-    <?php
-    session_start();
-                    $servername = 'localhost';
+        if (isset($_POST["login"]) && $_POST["login"] == "login") {
+            echo "Hello";
 
-                    if (isset($_POST["login"]) && $_POST["login"] == "login") {
-                        $username =  $_POST["user_username"];
-                        $password = $_POST["user_password"];
+            //     $username =  $_POST["user_username"];
+            //     $password = $_POST["user_password"];
 
-                        try {
-                            new PDO("mysql:host=$servername", $username, $password);
-                        } catch (PDOException $e) {}
-                    }
-   ?>
+            //     try {
+            //         new PDO("mysql:host=$servername", $username, $password);
+            //     } catch (PDOException $e) {}
+        }
+        ?>
+    </div>
     <script>
         // Function to handle the click event on the "show-login-reg" element
-        $("#show-login-reg").click(function () {
+        $("#show-login-reg").click(function() {
             // Show the login/register popup
             $("#login-register-popup").show();
 
             // Handle the switch to register form
-            $("#switch-to-register").click(function () {
+            $("#switch-to-register").click(function() {
                 // Hide the switch-to-register button and show the switch-to-login button
                 $("#switch-to-register").hide();
                 $("#switch-to-login").show();
@@ -165,7 +170,7 @@
             });
 
             // Handle the switch back to the login form
-            $("#switch-to-login").click(function () {
+            $("#switch-to-login").click(function() {
                 // Hide the switch-to-login button and show the switch-to-register button
                 $("#switch-to-login").hide();
                 $("#switch-to-register").show();
@@ -175,7 +180,7 @@
             });
 
             // Handle the switch to the forgot password form
-            $("#switch-to-forgotpass").click(function () {
+            $("#switch-to-forgotpass").click(function() {
                 // Hide both the login and register forms
                 $("#login-form").hide();
                 $("#register-form").hide();
@@ -187,14 +192,14 @@
             });
 
             // Handle the click event to close the popup
-            $("#close-popup").click(function () {
+            $("#close-popup").click(function() {
                 // Hide the login/register popup
                 $("#login-register-popup").hide();
             });
 
             // Handle file drop event for registration
             const fileInput = document.getElementById("customFile");
-            fileInput.addEventListener("drop", function (event) {
+            fileInput.addEventListener("drop", function(event) {
                 event.preventDefault();
                 const file = event.dataTransfer.files[0];
                 if (file) {
