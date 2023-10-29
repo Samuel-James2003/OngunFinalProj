aa62e3f0e102174a3d6e287abbeaf4c934a0f33e
Samuel James <64286580+Samuel-James2003@users.noreply.github.com>
2023-10-29 13:29:14 +0100

    Petite modif
    Jai fait des petit testes en nodejs. je crois qu'il va faloir completement changer l'appli mais bon...

--- VersionNodeJS/Server.js
+++ VersionNodeJS/Server.js
@@ -1,14 +1,40 @@
 const app = require("express")();
 const express = require("express");
+const bodyParser = require('body-parser');
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const portnum = 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Serve static files (e.g., HTML, CSS, JavaScript)
+app.use(express.static(__dirname + '/public'));
+
+// Login and Registration routes
+app.post('/login.php', (req, res) => {
+    // Handle the login request here
+    const email = req.body.log_email;
+    const password = req.body.log_password;
+    console.log(email);
+    // Authenticate the user, check the database, etc.
+    // Respond with appropriate data or redirect
+    res.send("Hi")
+});
+
+app.post('/register', (req, res) => {
+    // Handle the registration request here
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Validate and store user information in the database
+    // Respond with appropriate data or redirect
+});
 //Creation du serveur sur localhost
-app.use(express.static(__dirname + '/images'));
+app.use(express.static(__dirname + '/view'));
 
 app.get("/", (req, res) => {
-    res.sendFile(`${__dirname}/html/index.html`);
+    res.sendFile(`${__dirname}/view/index.php`);
+
 });
 
 io.on("connection", (socket) => {
