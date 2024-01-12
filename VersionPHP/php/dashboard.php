f18abac439733d2685d2e022d2492f54df844a63
Samuel James <64286580+Samuel-James2003@users.noreply.github.com>
2024-01-12 13:55:59 +0100

    Created html folder and about and contact

--- VersionPHP/php/dashboard.php
+++ VersionPHP/php/dashboard.php
@@ -7,61 +7,27 @@
     <title>Your Website</title>
     <!-- Add Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <style>
-        /* Styles for the overlay background of the popup */
-        #login-register-popup {
-            display: none;
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background: rgba(0, 0, 0, 0.6);
-            z-index: 999;
-        }
-
-        /* Styles for the content of the popup */
-        #login-register-popup .popup-content {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            background: #fff;
-            padding: 20px;
-        }
-
-        /* Styles for the close button within the popup */
-        .close-button {
-            position: absolute;
-            top: 5px;
-            right: 5px;
-            font-size: 24px;
-            background: none;
-            border: none;
-            cursor: pointer;
-        }
-    </style>
 
 </head>
 
 <body>
-    <!-- Navbar for both pages -->
+    <!--Todo : Modular navbar for pages -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">Your Website</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
+
+            <?php //todo : read the database and echo the right pages for the navbar and check if there is a submenu in that case do what you must
+            
+            ?>
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="index.html">Home</a>
+                    <a class="nav-link" href="../html/index.html">Home</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="dashboard.html">Dashboard</a>
-                </li>
-                <li class="nav-item">
-                    <button id="show-login-reg" class="btn btn-link">login</button>
-                </li>
+                
             </ul>
         </div>
     </nav>
@@ -101,10 +67,10 @@
         <input type="submit" value="Process Selected Rows">
     </div>
     <!-- TODO: Make a page for add(edit can go there too) -->
-    
-     <div>
+
+    <div>
         <button type="button" onclick="location.href='add_record.php'">Add</button>
-        <button type="button" >Delete</button>
+        <button type="button">Delete</button>
         <button type="button" onclick="location.href='edit_records.php'">Edit</button>
     </div>
     <!-- Add Bootstrap JS and jQuery for popup functionality -->
