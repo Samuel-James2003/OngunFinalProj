8aeaa6b4899ea4e54546ba38b31177389d14158b
Samuel James <64286580+Samuel-James2003@users.noreply.github.com>
2024-01-14 14:19:11 +0100

    petits changements de bootstrap

--- VersionPHP/php/login.php
+++ VersionPHP/php/login.php
@@ -174,6 +174,7 @@
                         $_SESSION['UserID'] = 1;
                         header("Location: ../php/dashboard.php");
                     }
+                    else{
                     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $pass);
 
                     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -190,6 +191,7 @@
                             break;
                         }
                     }
+                }
 
                 } catch (PDOException $e) {
                     Bootstrap_alert("danger", "Error", $e->getMessage());
